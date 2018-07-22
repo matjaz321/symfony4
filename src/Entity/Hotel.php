3c4f78618e25be47cbdb579b6e2f6a1c2903ca43
Unknown <matjaz.zavski15@gmail.com>
2018-07-22 14:31:47 +0200

    Created new fields: name, description, time, created_at for entity Hotel.

--- src/Entity/Hotel.php
+++ src/Entity/Hotel.php
@@ -3,21 +3,93 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraint as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\HotelRepository")
  */
-class Hotel
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+class Hotel {
 
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * @ORM\Id()
+   * @ORM\GeneratedValue()
+   * @ORM\Column(type="integer")
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(type="string")
+   * @Assert\NotBlank()
+   * @Assert\Length(min=5)
+   */
+  private $name;
+
+  /**
+   * @ORM\Column(type="text")
+   * @Assert\NotBlank()
+   * @Assert\Length(min=5)
+   */
+  private $description;
+
+  /**
+   * @ORM\Column(type="datetime")
+   * @Assert\NotBlank()
+   */
+  private $created_at;
+
+  /**
+   * @return mixed
+   */
+  public function getId() {
+    return $this->id;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @param mixed $name
+   */
+  public function setName($name): void {
+    $this->name = $name;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getDescription() {
+    return $this->description;
+  }
+
+  /**
+   * @param mixed $description
+   */
+  public function setDescription($description): void {
+    $this->description = $description;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getCreatedAt() {
+    return $this->created_at;
+  }
+
+  /**
+   * @param $created_at
+   */
+  public function setCreatedAt($created_at): void {
+    $this->created_at = $created_at;
+  }
+
+  /**
+   * @ORM\PrePersist()
+   */
+  public function setCreatedAtOnPersist(): void {
+    $this->created_at = new \DateTime();
+  }
 }

6bbd0a263fa3fbf8469f97e828a2bb589b1db011
matjaz321 <mza@customer-alliance.com>
2020-09-28 14:20:40 +0200

    oen more thing

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -1,90 +1,93 @@
-<?php
+    <?php
 
-namespace App\Controller;
+    namespace App\Controller;
 
-use App\CustomServices\CacheService;
-use App\Repository\HotelRepository;
-use App\Repository\ReviewRepository;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Twig\Cache\FilesystemCache;
+    use App\CustomServices\CacheService;
+    use App\Repository\HotelRepository;
+    use App\Repository\ReviewRepository;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Twig\Cache\FilesystemCache;
 
-/**
- * Class ReviewController
- *
- * @package \App\Controller
- */
-class ReviewController extends AbstractController {
+    /**
+     * Class ReviewController
+     *
+     * @package \App\Controller
+     */
+    class ReviewController extends AbstractController {
 
-  /**
-   * @var \App\Repository\ReviewRepository
-   */
-  private $reviewRepository;
+      /**
+       * @var \App\Repository\ReviewRepository
+       */
+      private $reviewRepository;
 
-  /**
-   * @var \App\Repository\HotelRepository
-   */
-  private $hotelRepository;
+      /**
+       * @var \App\Repository\HotelRepository
+       */
+      private $hotelRepository;
 
-  /**
-   * @var \App\CustomServices\CacheService
-   */
-  private $cacheService;
+      /**
+       * @var \App\CustomServices\CacheService
+       */
+      private $cacheService;
 
-  /**
-   * ReviewController constructor.
-   *
-   * @param \App\Repository\ReviewRepository $reviewRepository
-   * @param \App\Repository\HotelRepository  $hotelRepository
-   * @param \App\CustomServices\CacheService $cacheService
-   */
-  public function __construct(
-    ReviewRepository $reviewRepository,
-    HotelRepository $hotelRepository,
-    CacheService $cacheService
-  ) {
-    $this->reviewRepository = $reviewRepository;
-    $this->hotelRepository = $hotelRepository;
-    $this->cacheService = $cacheService;
-  }
+      /**
+       * ReviewController constructor.
+       *
+       * @param \App\Repository\ReviewRepository $reviewRepository
+       * @param \App\Repository\HotelRepository  $hotelRepository
+       * @param \App\CustomServices\CacheService $cacheService
+       */
+      public function __construct(
+        ReviewRepository $reviewRepository,
+        HotelRepository $hotelRepository,
+        CacheService $cacheService
+      ) {
+        $this->reviewRepository = $reviewRepository;
+        $this->hotelRepository = $hotelRepository;
+        $this->cacheService = $cacheService;
+      }
 
-  /**
-   * @Route("/{hotelId}/today/review", name="review_random")
-   */
-  public function showRandomReview($hotelId) {
-    // Check if hotel with this ID exists in the database.
-    if (!($hotel = $this->hotelRepository->find($hotelId))) {
-      throw $this->createNotFoundException("The hotel $hotelId was not found.");
+      /**
+       * @Route("/{hotelId}/today/review", name="review_random")
+       */
+      public function showRandomReview($hotelId) {
+        // Check if hotel with this ID exists in the database.
+        if (!($hotel = $this->hotelRepository->find($hotelId))) {
+          throw $this->createNotFoundException("The hotel $hotelId was not found.");
+        }
+
+        // Get cached review.
+        $randomReview = $this->cacheService->getRandomReview($hotel);
+        // Display this review.
+        return $this->render('reviews/random-review.html.twig', [
+          'review' => $randomReview,
+        ]);
+      }
+
+
+      /**
+       * @Route("/{hotelId}/today/reviasfA"asgA"SGasew", name="review_random")
+       */
+      public function showRandomReview($hotelId) {
+        // Check if hotel with this ID exists in the database.
+        asgsagasgasgasgsag
+        if (!($hotel = $this->hotelRepository->find($hotelId))) {
+          throw $this->createNotFoundException("The hotel $hotelId was not found.");
+        }
+
+        $b = $request->get('name');
+
+        $test = "SELECT * from users WHERE name = $b";
+
+        $this->getRepository(HotelRepository::class)->find(1);
+
+        // Get cached review.
+        $randomReview = $this->cacheService->getRandomReview($hotel);
+        // Display this review.
+        return $this->render('reviews/random-review.html.twig', [
+          'review' => $randomReview,
+        ]);
+        
+      }
     }
-
-    // Get cached review.
-    $randomReview = $this->cacheService->getRandomReview($hotel);
-    // Display this review.
-    return $this->render('reviews/random-review.html.twig', [
-      'review' => $randomReview,
-    ]);
-  }
-
-
-  /**
-   * @Route("/{hotelId}/today/reviasfA"asgA"SGasew", name="review_random")
-   */
-  public function showRandomReview($hotelId) {
-    // Check if hotel with this ID exists in the database.
-    asgsagasgasgasgsag
-    if (!($hotel = $this->hotelRepository->find($hotelId))) {
-      throw $this->createNotFoundException("The hotel $hotelId was not found.");
-    }
-
-    $b = $request->get('name');
-
-    $test = "SELECT * from users WHERE name = $b";
-
-    // Get cached review.
-    $randomReview = $this->cacheService->getRandomReview($hotel);
-    // Display this review.
-    return $this->render('reviews/random-review.html.twig', [
-      'review' => $randomReview,
-    ]);
-  }
-}
